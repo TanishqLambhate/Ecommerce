37a5cf7447dcc6712fed7b0f3eb4ee3b4ecce954
TanishqLambhate <tanulambhate@gmail.com>
2023-03-07 21:28:10 +0530

    second commit

--- ecomsite/settings.py
+++ ecomsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')v%)ysrv-=o-8vk!!akvhk4@m0%gmb)-z11*5ikix#4n%g(+@h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
